570422bd43fb87ba346ff9b3d5d3687a5a4c94ef
Jared Stimson <36967391+jmstimso@users.noreply.github.com>
2018-07-13 13:26:46 -0400

    Initial checkin for active auth server

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-import adal
 import flask
 import uuid
 import requests
 import config
+import json
+import jwt
+import base64
+from cryptography.x509 import load_der_x509_certificate
+from cryptography.hazmat.backends import default_backend
 
 app = flask.Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
 
 PORT = 5000  # A flask app by default runs on PORT 5000
-AUTHORITY_URL = config.AUTHORITY_HOST_URL + '/' + config.TENANT
-REDIRECT_URI = 'http://localhost:{}/getAToken'.format(PORT)
-TEMPLATE_AUTHZ_URL = ('https://login.microsoftonline.com/{}/oauth2/authorize?' +
-                      'response_type=code&client_id={}&redirect_uri={}&' +
-                      'state={}&resource={}')
+AUTHORITY_URL = config.AUTHORITY_HOST_URL + '/dstsv2/' + config.TENANT
+REDIRECT_URI = 'http://localhost:{}/displaytoken'.format(PORT)
+TEMPLATE_AUTHZ_URL = ('{}/dstsv2/{}/oauth2/authorize?' +
+                      'response_type={}&client_id={}&redirect_uri={}&' +
+                      'state={}&resource={}&response_mode={}&nonce={}')
 
-
+def get_sign_cert(accessToken: str):
+		endpoint = config.AUTHORITY_HOST_URL + '/dstsv2/common/discovery/keys'
+		http_headers = {'User-Agent': 'adal-python-sample','Accept': 'application/json', 'Content-Type': 'application/json','client-request-id': str(uuid.uuid4())}
+		sign_key = requests.get(endpoint, headers=http_headers, stream=False).json()
+		certificate_text = ''
+		for key in sign_key['keys']:
+				if key['kid'] == jwt.get_unverified_header(accessToken)['kid']:
+						certificate_text = base64.b64decode(key['x5c'][0])
+						break
+		if certificate_text == '' : return certificate_text
+		certificate = load_der_x509_certificate(certificate_text, default_backend())
+		return certificate.public_key()
+		
+def verify_iss(iss: str):
+		for domain in config.DSTS_DOMAINS :
+				if iss == domain :
+						return True
+		return False
+		
 @app.route("/")
 def main():
     login_url = 'http://localhost:{}/login'.format(PORT)
@@ -27,45 +49,47 @@ def main():
 @app.route("/login")
 def login():
     auth_state = str(uuid.uuid4())
+    nonce = str(uuid.uuid4())
     flask.session['state'] = auth_state
     authorization_url = TEMPLATE_AUTHZ_URL.format(
-        config.TENANT,
+    		config.AUTHORITY_HOST_URL,
+    		config.TENANT,
+        config.RESPONCE_TYPE,
         config.CLIENT_ID,
         REDIRECT_URI,
         auth_state,
-        config.RESOURCE)
+        config.RESOURCE,
+        config.RESPONCE_MODE,
+        nonce)
     resp = flask.Response(status=307)
     resp.headers['location'] = authorization_url
     return resp
 
 
-@app.route("/getAToken")
+@app.route("/displaytoken", methods=['GET', 'POST'])
 def main_logic():
-    code = flask.request.args['code']
-    state = flask.request.args['state']
-    if state != flask.session['state']:
-        raise ValueError("State does not match")
-    auth_context = adal.AuthenticationContext(AUTHORITY_URL)
-    token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, config.RESOURCE,
-                                                                        config.CLIENT_ID, config.CLIENT_SECRET)
-    # It is recommended to save this to a database when using a production app.
-    flask.session['access_token'] = token_response['accessToken']
+		if flask.request.method == 'POST' :
+				id_token = flask.request.form['id_token']
+		else:
+				id_token = flask.request.args['id_token']
+		flask.session['id_token'] = id_token
+		public_key = get_sign_cert(id_token)
+		if public_key == '' : return 'Could not find certificate to validate token.', 500
+		decoded_jwt = jwt.decode(id_token, public_key, audience=config.CLIENT_ID)
+		iss = decoded_jwt['iss'].split('/')[2]
+		if not verify_iss(iss): return 'Token not issued by a trusted authority.', 500
+		return flask.render_template('display_token_info.html', id_token=json.dumps(decoded_jwt, indent=2)), 200
 
-    return flask.redirect('/graphcall')
-
-
-@app.route('/graphcall')
-def graphcall():
-    if 'access_token' not in flask.session:
-        return flask.redirect(flask.url_for('login'))
-    endpoint = config.RESOURCE + '/' + config.API_VERSION + '/me/'
-    http_headers = {'Authorization': flask.session.get('access_token'),
-                    'User-Agent': 'adal-python-sample',
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'client-request-id': str(uuid.uuid4())}
-    graph_data = requests.get(endpoint, headers=http_headers, stream=False).json()
-    return flask.render_template('display_graph_info.html', graph_data=graph_data)
+@app.route('/echo')
+def echo_resp():
+		access_token = flask.request.headers.get('Authorization')
+		flask.session['access_token'] = access_token
+		public_key = get_sign_cert(access_token)
+		if public_key == '' : return 'Could not find certificate to validate token.', 500
+		decoded_jwt = jwt.decode(access_token, public_key, audience=config.CLIENT_ID)
+		iss = decoded_jwt['iss'].split('/')[2]
+		if not verify_iss(iss): return 'Token not issued by a trusted authority.', 500
+		return json.dumps(decoded_jwt, indent=2), 200
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -1,8 +1,16 @@
-RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
-TENANT = "Your tenant"  # Enter tenant name, e.g. contoso.onmicrosoft.com
-AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
-CLIENT_ID = "Your client id "  # copy the Application ID of your app from your Azure portal
-CLIENT_SECRET = "Your client secret"  # copy the value of key you generated when setting up the application
+RESOURCE = "http://pythonwebapi.jastimso-dev"  # Add the resource you want the access token for
+TENANT = "7A433BFC-2514-4697-B467-E0933190487F"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+AUTHORITY_HOST_URL = "https://co1-test7-dsts.dsts.core.azure-test.net"
+CLIENT_ID = "9d855746-3b9b-4dd7-9517-e102cebeb285"  # copy the Application ID of your app from your Azure portal
+RESPONCE_TYPE = "id_token code" #
+RESPONCE_MODE = "form_post" # responce_mode is either form_post or query
 
+DSTS_DOMAINS = [
+        'dsts.core.windows.net',
+        'dsts.core.chinacloudapi.cn',  
+        'dsts.core.cloudapi.de', 
+        'dsts.core.usgovcloudapi.net',  
+        'dsts.core.azure-test.net',
+        ]
 # These settings are for the Microsoft Graph API Call
 API_VERSION = 'v1.0'
